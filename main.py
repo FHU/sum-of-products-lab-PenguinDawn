c786354720c0ce624c0cbbf8c482ac8bef126cfa
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2024-01-25 15:16:13 -0600

    Update main.py
    Fixed how to receive the tests

--- main.py
+++ main.py
@@ -6,18 +6,24 @@ def sum_of_products(list1, list2):
 
     if len(list1) != len(list2):
         print("error")
+        return 0
+    
     for i in range(0, len(list1)):
         newNum = int(list1[i]) * int(list2[i])
-        print(newNum)
         finalList.append(newNum)
+
     lastNum = 0
     for x in finalList:
         lastNum += x
     
-    print(lastNum) 
+    print(lastNum)
 
 print(sum_of_products([1, 2, 3], [3, 2, 1]))
 if __name__ == '__main__':
-   input1 = int(input())
-   input2 = int(input())
-   sum_of_products(input1, input2)
+   input1 = input()
+   input2 = input()
+   input1 = input1[0:-1]
+   input2 = input2[0:-1]
+   list1 = input1.split(" ")
+   list2 = input2.split(" ")
+   sum_of_products(list1, list2)
