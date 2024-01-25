434f47e49b4ab20cace2aecb4c1443b15b3e21a4
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2024-01-25 10:04:54 -0600

    Update main.py
    This is the code I had for now. It compares list length and also adds the sum of the products of the matching numbers.

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
+#REMOVE PASS AND FIX THE FUNCTION
 def sum_of_products(list1, list2):
-    pass
+    finalList = []
+
+    if len(list1) != len(list2):
+        return "ERROR"
+    for i in list1:
+        for y in list2:
+            newNum = int(i) * int(y)
+            finalList.append(newNum)
+
+    lastNum = 0
+    for x in finalList:
+        lastNum += x
+    
+    return lastNum 
 
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
